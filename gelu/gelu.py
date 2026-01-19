4ad0da3efd6dce545010033049365836720fc5e9
__Maxim__ <bogdanovmax41@gmail.com>
2026-01-19 05:57:05 +0300

    Add solution for gelu

---
+++ gelu/gelu.py
@@ -0,0 +1,12 @@
+import numpy as np
+import math as m
+
+def gelu(x):
+    """
+    Compute the Gaussian Error Linear Unit (exact version using erf).
+    x: scalar, list, or np.ndarray
+    Return: np.ndarray of same shape (dtype=float)
+    """
+    x = np.asarray(x, dtype=float)
+    erf_ = np.vectorize(math.erf)
+    return 0.5 * x * (1 + erf_(x / np.sqrt(2)))
